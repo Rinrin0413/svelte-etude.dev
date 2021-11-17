0855debd7560ddcd09cb71c57373eeabe30c564b
Rinrin0413 <67325022+Rinrin0413@users.noreply.github.com>
2021-11-17 17:52:10 +0900

    一新

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,24 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+    const na = "那";
+    const sbelde = "https://svelte.jp/svelte-logo-horizontal.svg";
+    const tlykeshy = "<strong>AAA</strong>";
+    let dot = ".";
+    function boo() { dot += "."; }
+    $: excl = dot.length;
+    $: if(dot.length === 10) {
+        alert("BOO!!!!!!!!!!")
+    }
+</script>
+<style lang="scss">
+    $main_clr:tomato;
+    img { height: 64px; border: darkgreen 5px solid; }
+    h1 { color: $main_clr; font-size: 2em; }
+    span { text-decoration: line-through; }
+</style>
+
+<h1><span>お{na}べ</span>構文{dot === "." ? "( 'ω')" : dot === ".." ? "('ω' )": dot}</h1>
+
+<button on:click={boo}>boo!!({excl})</button>
+<br>
+<img src={sbelde} alt="ⓈSBELDE">
+<p>{tlykeshy} || -> || {@html tlykeshy}</p>
